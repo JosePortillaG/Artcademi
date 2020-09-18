18834bd31b12a8ead3b5c06ae841b373c73c2e42
Jose Portilla <j1990eternal@gmail.com>
2020-09-18 12:58:50 -0300

    Updates

---
+++ precache-manifest.b53fdcc5706b278f24eb28574ef8ff63.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bba430860898639da9fdbdbc86a33e",
+    "url": "/Artcademi/index.html"
+  },
+  {
+    "revision": "9c8155f0090687592c09",
+    "url": "/Artcademi/static/css/main.c2b42059.chunk.css"
+  },
+  {
+    "revision": "b7f892c59232b1e500ce",
+    "url": "/Artcademi/static/js/2.86877fa7.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/Artcademi/static/js/2.86877fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8155f0090687592c09",
+    "url": "/Artcademi/static/js/main.4fee9822.chunk.js"
+  },
+  {
+    "revision": "4ff901eb885c9f34d7c0",
+    "url": "/Artcademi/static/js/runtime-main.45c12be9.js"
+  },
+  {
+    "revision": "3c4c7923323dc5311cdca847a8dc0574",
+    "url": "/Artcademi/static/media/LatinaEssential-Bold.3c4c7923.woff2"
+  },
+  {
+    "revision": "a1137ab7d724e52887fb95e014fa47c6",
+    "url": "/Artcademi/static/media/LatinaEssential-HeavyIt.a1137ab7.woff2"
+  },
+  {
+    "revision": "aa13941a8f20e19f37286e2e32c309f0",
+    "url": "/Artcademi/static/media/LatinaEssential-LightIt.aa13941a.woff2"
+  },
+  {
+    "revision": "917fe6c1264836a81c2966f5c1c3ae10",
+    "url": "/Artcademi/static/media/LatinaEssential-Medium.917fe6c1.woff2"
+  },
+  {
+    "revision": "f6f6bbf32be1b4ee23945f48a4910661",
+    "url": "/Artcademi/static/media/LatinaEssential-MediumIt.f6f6bbf3.woff2"
+  },
+  {
+    "revision": "c8d77d3eccf2cf9962956138746e9cd8",
+    "url": "/Artcademi/static/media/PaytoneOne-Regular.c8d77d3e.ttf"
+  },
+  {
+    "revision": "923f002f8209d7e28830e32943703ad4",
+    "url": "/Artcademi/static/media/artcademi-artistaslogo.923f002f.svg"
+  },
+  {
+    "revision": "e4c51bd86e49d4407e201718feb2758e",
+    "url": "/Artcademi/static/media/artcademi-azulescudo.e4c51bd8.png"
+  },
+  {
+    "revision": "66f6c9d05ddce6642e068221a55ee196",
+    "url": "/Artcademi/static/media/artcademi-backblogX2.66f6c9d0.png"
+  },
+  {
+    "revision": "d17476d739a2ad1b93ea998bb6aae420",
+    "url": "/Artcademi/static/media/artcademi-bulletcomX2.d17476d7.png"
+  },
+  {
+    "revision": "c731012f729c1833c18cce963a4f68fa",
+    "url": "/Artcademi/static/media/artcademi-fondo1X2.c731012f.png"
+  },
+  {
+    "revision": "c824a5df437726f417dfaa5098677c68",
+    "url": "/Artcademi/static/media/artcademi-fondojuniorX2.c824a5df.png"
+  },
+  {
+    "revision": "a98f9d8c04afdef5dbffb949dfe48c90",
+    "url": "/Artcademi/static/media/artcademi-iniciobuscarX2.a98f9d8c.png"
+  },
+  {
+    "revision": "db8de13db84c2880adca014d6030f28e",
+    "url": "/Artcademi/static/media/artcademi-iniciocomunica.db8de13d.png"
+  },
+  {
+    "revision": "56e80551912e78ed9b8950160ac83e1f",
+    "url": "/Artcademi/static/media/artcademi-iniciocomunicarX2.56e80551.png"
+  },
+  {
+    "revision": "6b98d328ef03372eedf78addbc3a4c8f",
+    "url": "/Artcademi/static/media/artcademi-iniciotip.6b98d328.png"
+  },
+  {
+    "revision": "9acd178c273e42bd03dfe08a49e2334d",
+    "url": "/Artcademi/static/media/artcademi-iniciotipX2.9acd178c.png"
+  },
+  {
+    "revision": "e9f3dd99f02206e391926fe19351524a",
+    "url": "/Artcademi/static/media/artcademi-jdilla-X2.e9f3dd99.png"
+  },
+  {
+    "revision": "8fb938462dbe6645d39006ab44231a2d",
+    "url": "/Artcademi/static/media/artcademi-logo.8fb93846.svg"
+  },
+  {
+    "revision": "4cd4df5093997e11e90cea61b12dcfdb",
+    "url": "/Artcademi/static/media/artcademi-logodfhilo.4cd4df50.png"
+  },
+  {
+    "revision": "c9f9e38c2aee832e9f8f58393ff29ea9",
+    "url": "/Artcademi/static/media/artcademi-logodfhiloX2.c9f9e38c.png"
+  },
+  {
+    "revision": "dba88808efba3cff0eae98b80db0147f",
+    "url": "/Artcademi/static/media/artcademi-manito.dba88808.png"
+  },
+  {
+    "revision": "7825491e8b59716554494450e8115d15",
+    "url": "/Artcademi/static/media/artcademi-manitoX2.7825491e.png"
+  },
+  {
+    "revision": "9da187e606a0dcf22e276bb637c6bcf9",
+    "url": "/Artcademi/static/media/artcademi-modeloArtistaApp.9da187e6.png"
+  },
+  {
+    "revision": "0407ceff6acc964368386219343a6abc",
+    "url": "/Artcademi/static/media/artcademi-modeloArtistadFhilo.0407ceff.png"
+  },
+  {
+    "revision": "abce32c45c222a561afadbb4087cd69b",
+    "url": "/Artcademi/static/media/artcademi-naranjaescudo.abce32c4.png"
+  },
+  {
+    "revision": "040225ae9255dbcc9acf791a025b0f4f",
+    "url": "/Artcademi/static/media/artcademi-ninjart.040225ae.png"
+  },
+  {
+    "revision": "3bc669d0ec001b1285498c448a905141",
+    "url": "/Artcademi/static/media/artcademi-prototipoX2.3bc669d0.png"
+  },
+  {
+    "revision": "c065ecd956e2358125e8b41c8fd2d66a",
+    "url": "/Artcademi/static/media/artcademi-prototipodfhiloX2.c065ecd9.png"
+  },
+  {
+    "revision": "e8e8c530160f9b30701b520b0f23a37b",
+    "url": "/Artcademi/static/media/artcademi-purpuraescudo.e8e8c530.png"
+  },
+  {
+    "revision": "5317182cdfe17ff3f1996867c871fd48",
+    "url": "/Artcademi/static/media/dfhilo-hoz.5317182c.png"
+  },
+  {
+    "revision": "0e4ffd319e2fb3f2bb6dd52da3924d73",
+    "url": "/Artcademi/static/media/fondo-hero-right.0e4ffd31.png"
+  },
+  {
+    "revision": "4ad01d7f82126cea2206a464bb2a5bf8",
+    "url": "/Artcademi/static/media/fondo-patreon.4ad01d7f.png"
+  },
+  {
+    "revision": "3d263a705892dcb3cb8357a1559a6d72",
+    "url": "/Artcademi/static/media/hero-fondo-768.3d263a70.png"
+  },
+  {
+    "revision": "133d2a326d939e74dec4e41f3e5eab31",
+    "url": "/Artcademi/static/media/hero-fondo.133d2a32.png"
+  },
+  {
+    "revision": "75426ef10c7b55cd633e5a3c1c227382",
+    "url": "/Artcademi/static/media/icon-instagram.75426ef1.png"
+  },
+  {
+    "revision": "a537d45827bdf671504cf2a73bb3673b",
+    "url": "/Artcademi/static/media/img-error-404.a537d458.png"
+  },
+  {
+    "revision": "6a31db6e862f2ca48fd4075e70e021f6",
+    "url": "/Artcademi/static/media/landing-jdilla.6a31db6e.png"
+  },
+  {
+    "revision": "16f7ba4bdfc3d5c103ec5deb2c60741e",
+    "url": "/Artcademi/static/media/logo-patreon.16f7ba4b.svg"
+  }
+]);
